a8af2ac32e26876f24f6dca30bc83cea4d4d31cf
natoinet <antoine.brunel@gmail.com>
2018-08-03 20:44:59 +0200

    Compiling but celerybeat, gunicorn, postgresql & rabbitmq-server are failing to start

--- Dockerfile
+++ Dockerfile
@@ -2,46 +2,60 @@
 # Tucat source code https://github.com/natoinet/tucat 
 # sudo docker build -t natoinet/tucat . 
 
-FROM debian:jessie 
-MAINTAINER Victor Esteban <victor@limogin.com>
+FROM debian:stretch 
+MAINTAINER Antoine Brunel <antoine.brunel@gmail.com>
 LABEL release=1.0 
 
 # Install packages
 ENV DEBIAN_FRONTEND noninteractive
-ENV APPHOME /home/antoinet/src/
-ENV APPUSER antoinet 
-ENV SERVERNAME tucat.garbellador.com 
+ENV APPUSER antoinet
+ENV USERHOME /home/antoinet
+ENV APPHOME /home/antoinet/src
+ENV APPLOG /var/log
+ENV APPVENV /home/antoinet/.virtualenvs/tucat
+ENV SERVERNAME tucat.garbellador.com
 
 # Base Debian, Python and PostGreSQL packages 
 RUN echo "Install base system" && \
     apt-get update && \
     apt-get -y install supervisor git pwgen libssl-dev openssl openssh-server build-essential \
     python python3-setuptools python3-dev virtualenv python-virtualenv python3-virtualenv python3-pip python-pip nginx gunicorn vim nano curl wget \
-    postgresql postgresql-contrib libpq-dev python-software-properties software-properties-common postgresql-client postgresql-contrib apt-utils net-tools \
+    postgresql postgresql-contrib libpq-dev software-properties-common postgresql-client postgresql-contrib apt-utils net-tools \
     python-dev python3-dev virtualenvwrapper dh-virtualenv devscripts git equivs && \
-    pip install django && pip3 install django && \
+    pip install Django==1.9.5 && pip3 install Django==1.9.5 && \
     pip install boto && pip3 install boto && \
-    mkdir -p /var/run/postgresql/9.4-main.pg_stat_tmp && \
-    chown -R postgres.postgres /var/run/postgresql/9.4-main.pg_stat_tmp 
-    # easy_install pip python-dev python3-dev && \
+    mkdir -p /var/run/postgresql/9.6-main.pg_stat_tmp && \
+    chown -R postgres.postgres /var/run/postgresql/9.6-main.pg_stat_tmp 
+    #easy_install pip python-dev python3-dev && \
 
 # Install MongoDB database 
 RUN echo "Install MongoDB database" && \
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 && \
-    echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list && \
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 && \
+    echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list && \
     apt-get update && \
     apt-get install -y mongodb-org 
+
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 && \
+#echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list && \
+#echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.7 main" | tee /etc/apt/sources.list.d/mongodb-org-3.7.list && \
+
 
 # Install RabbitMQ Queue Service 
 RUN echo "Install RabbitMQ queue service" && \
     echo 'deb http://www.rabbitmq.com/debian/ testing main' >> /etc/apt/sources.list && \
-    curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add - && \
+    curl https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add - && \
     apt-get update && \
     apt-get -y install rabbitmq-server 
+
+RUN echo "Python version " && python --version
+RUN echo "pip list " && pip list | grep setuptools
+RUN echo "Python 3 version " && python3 --version
+RUN echo "pip3 list " && pip3 list | grep setuptools
 
 # Install Tucat Application 
 ADD ./nginx.conf /etc/nginx/sites-available/${SERVERNAME}
 ADD ./requirements.txt /requirements.txt 
+#ADD https://raw.githubusercontent.com/natoinet/tucat/master/requirements.txt /requirements.txt
 RUN echo "Install Tucat Application " && \
     useradd -ms /bin/bash ${APPUSER} && \
     git clone https://github.com/natoinet/tucat /src/tucat && \
@@ -49,7 +63,7 @@ RUN echo "Install Tucat Application " && \
     ln -s /etc/nginx/sites-available/${SERVERNAME} /etc/nginx/sites-enabled/ && \
     mkdir -p ${APPHOME} && \
     cp -r /src/tucat/ ${APPHOME}/tucat && \
-    mkdir -p /home/antoinet/src/tucat/log && \
+    mkdir -p ${APPHOME}/tucat/log && \
     echo "daemon off;" >> /etc/nginx/nginx.conf 
        
 # Supervisor configuration 
@@ -67,8 +81,11 @@ RUN mkdir -p /var/run/sshd
 RUN echo 'root:tucatpwd' | chpasswd
 RUN sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
-RUN echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.4/main/pg_hba.conf
-RUN echo "listen_addresses='*'" >> /etc/postgresql/9.4/main/postgresql.conf
+RUN echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.6/main/pg_hba.conf
+RUN echo "listen_addresses='*'" >> /etc/postgresql/9.6/main/postgresql.conf
+
+RUN echo "Python version " && python --version
+RUN echo "pip list " && pip list | grep setuptools
 
 # RABBIT ADMIN
 RUN wget -c https://raw.githubusercontent.com/rabbitmq/rabbitmq-management/rabbitmq_v3_6_1/bin/rabbitmqadmin && \
--- init.sh
+++ init.sh
@@ -12,15 +12,17 @@ echo "PSQL_PASSWORD=${PASS} " >> /root/.passwd
 # echo "DJANGO_SETTINGS_MODULE=config.settings.production" >> ${APPHOME}/tucat/.env
 # echo "DATABASE_URL=postgres://garbellador:${PASS}@localhost:5432/dj_tucat" >> ${APPHOME}/tucat/.env 
 
-# su -l postgres -c '/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf' 
+# su -l postgres -c '/usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf' 
 service postgresql start 
 
 echo "create role admin superuser login;" > /init_postgresql.sql 
 echo "create user debian with superuser password '${PASS}';" >> /init_postgresql.sql 
 echo "grant admin to debian; " >> /init_postgresql.sql 
-echo "create user garbellador with password '${PASS}'; " >> /init_postgresql.sql 
+#echo "create user garbellador with password '${PASS}'; " >> /init_postgresql.sql
+echo "create user ${APPUSER} with password '${PASS}'; " >> /init_postgresql.sql
 echo "create database dj_tucat; " >> /init_postgresql.sql 
-echo "grant all privileges on database dj_tucat to garbellador; " >> /init_postgresql.sql  
+#echo "grant all privileges on database dj_tucat to garbellador; " >> /init_postgresql.sql  
+echo "grant all privileges on database dj_tucat to ${APPUSER}; " >> /init_postgresql.sql  
 
 echo "Waiting to start progresql service"
 
@@ -29,7 +31,8 @@ echo "Waiting to start progresql service"
 su -l postgres -c 'psql < /init_postgresql.sql'
 
 rm -rf /src/tucat 
-chown -R antoinet.antoinet /home/antoinet/src 
+#chown -R antoinet:antoinet /home/antoinet/src 
+chown -R ${APPUSER}:${APPUSER} ${APPHOME} 
 rm -f /init_postgresql.sql
 
 ulimit -n 1024
@@ -37,35 +40,51 @@ mkdir -p /var/lib/rabbitmq/data
 chown -R rabbitmq.rabbitmq /var/lib/rabbitmq/data
 mkdir -p /var/log/rabbitmq/
 
-cp /requirements.txt /home/antoinet/requirements.txt 
-chown -R antoinet.antoinet /home/antoinet/requirements.txt 
+#cp /requirements.txt /home/antoinet/requirements.txt 
+cp /requirements.txt ${USERHOME}/requirements.txt 
+#chown -R antoinet:antoinet /home/antoinet/requirements.txt 
+chown -R ${APPUSER}:${APPUSER} ${USERHOME}/requirements.txt 
 rm -f /requirements.txt
 
 DJANGO_SECRET_KEY=$(pwgen -s 12 1)
 echo "DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}" >> /root/.passwd 
 
-echo "DEBUG=on" > /home/antoinet/src/tucat/.env
-echo "DJANGO_SETTINGS_MODULE=config.settings.test" >> /home/antoinet/src/tucat/.env
-echo "DATABASE_URL=postgres://garbellador:${PASS}@localhost:5432/dj_tucat" >> /home/antoinet/src/tucat/.env
-echo "DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}" >> /home/antoinet/src/tucat/.env
-echo "SECRET_KEY=${DJANGO_SECRET_KEY}" >> /home/antoinet/src/tucat/.env
+#echo "DEBUG=on" > /home/antoinet/src/tucat/.env
+echo "DEBUG=on" > ${APPHOME}/tucat/.env
+#echo "DJANGO_SETTINGS_MODULE=config.settings.test" >> /home/antoinet/src/tucat/.env
+echo "DJANGO_SETTINGS_MODULE=config.settings.test" >> ${APPHOME}/tucat/.env
+#echo "DATABASE_URL=postgres://garbellador:${PASS}@localhost:5432/dj_tucat" >> /home/antoinet/src/tucat/.env
+echo "DATABASE_URL=postgres://${APPUSER}:${PASS}@localhost:5432/dj_tucat" >> ${APPHOME}/tucat/.env
+#echo "DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}" >> /home/antoinet/src/tucat/.env
+echo "DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}" >> ${APPHOME}/tucat/.env
+#echo "SECRET_KEY=${DJANGO_SECRET_KEY}" >> /home/antoinet/src/tucat/.env
+echo "SECRET_KEY=${DJANGO_SECRET_KEY}" >> ${APPHOME}/tucat/.env
 
-echo '#!/bin/bash' > /home/antoinet/init.sh
-echo "export WORKON_HOME=/home/antoinet/.virtualenvs" >> /home/antoinet/init.sh
-echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> /home/antoinet/init.sh
-echo "mkvirtualenv --python=/usr/bin/python3 -r /home/antoinet/requirements.txt tucat" >> /home/antoinet/init.sh
-echo "source .virtualenvs/tucat/bin/activate; pip install boto; pip install django; pip install gunicorn; export DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}; mkdir -p /home/antoinet/log/" >> /home/antoinet/init.sh
-chown antoinet.antoinet /home/antoinet/init.sh
-chmod +x /home/antoinet/init.sh
+#echo '#!/bin/bash' > /home/antoinet/init.sh
+echo '#!/bin/bash' > ${USERHOME}/init.sh
+#echo "export WORKON_HOME=/home/antoinet/.virtualenvs" >> /home/antoinet/init.sh
+echo "export WORKON_HOME=${USERHOME}/.virtualenvs" >> ${USERHOME}/init.sh
+#echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> /home/antoinet/init.sh
+echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> ${USERHOME}/init.sh
+#echo "mkvirtualenv --python=/usr/bin/python3 -r /home/antoinet/requirements.txt tucat" >> /home/antoinet/init.sh
+echo "mkvirtualenv --python=/usr/bin/python3 -r ${USERHOME}/requirements.txt tucat" >> ${USERHOME}/init.sh
+#echo "source .virtualenvs/tucat/bin/activate; pip install boto; pip install django; pip install gunicorn; export DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}; mkdir -p /home/antoinet/log/" >> /home/antoinet/init.sh
+echo "source .virtualenvs/tucat/bin/activate; pip install boto; pip install django; pip install gunicorn; export DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}; mkdir -p ${USERHOME}/log/" >> ${USERHOME}/init.sh
+#chown antoinet:antoinet /home/antoinet/init.sh
+chown ${APPUSER}:${APPUSER} ${USERHOME}/init.sh
+#chmod +x /home/antoinet/init.sh
+chmod +x ${USERHOME}/init.sh
 
-su -l antoinet -c '/home/antoinet/init.sh'
+#su -l antoinet -c '/home/antoinet/init.sh'
+su -l ${APPUSER} -c "${USERHOME}/init.sh"
 
 service postgresql stop 
 
 /etc/init.d/rabbitmq-server start
 
 RBPASS=$(pwgen -s 12 1)
-echo "RABBIT_USER=admin" >> /root/.passwd 
+#echo "RABBIT_USER=admin" >> /root/.passwd 
+echo "RABBIT_USER=${APPUSER}" >> /root/.passwd 
 echo "RABBIT_PASSWORD=${RBPASS}" >> /root/.passwd 
 
 rabbitmq-plugins enable rabbitmq_management 
@@ -76,22 +95,30 @@ rabbitmq-plugins enable rabbitmq_management_visualiser
 #rabbitmqctl set_user_tags admin management
 #rabbitmqctl set_permissions -p / admin ".*" ".*" ".*" 
 
-rabbitmqctl add_user tucat ${RBPASS}
-rabbitmqctl set_user_tags tucat administrator
-rabbitmqctl set_user_tags tucat management
-rabbitmqctl set_permissions -p / tucat ".*" ".*" ".*" 
+#rabbitmqctl add_user admin ${RBPASS}
+rabbitmqctl add_user ${APPUSER} ${RBPASS}
+#rabbitmqctl set_user_tags admin administrator
+rabbitmqctl set_user_tags ${APPUSER} administrator
+#rabbitmqctl set_user_tags admin management
+rabbitmqctl set_user_tags ${APPUSER} management
+#rabbitmqctl set_permissions -p / admin ".*" ".*" ".*" 
+rabbitmqctl set_permissions -p / ${APPUSER} ".*" ".*" ".*" 
 
-rabbitmqctl add_vhost tucat.garbellador.com
-rabbitmqctl set_user_tags tucat tucat 
+#rabbitmqctl add_vhost tucat.garbellador.com
+rabbitmqctl add_vhost ${SERVERNAME}
+#rabbitmqctl set_user_tags tucat tucat
+rabbitmqctl set_user_tags ${APPUSER} ${APPUSER} 
 
-rabbitmqadmin declare queue name=tucat durable=true 
+#rabbitmqadmin declare queue name=tucat durable=true 
+rabbitmqadmin declare queue name=${APPUSER} durable=true 
 
 /etc/init.d/rabbitmq-server stop 
 
-sed -i "s#BROKER_URL.*#BROKER_URL = 'amqp://tucat:${RBPASS}@localhost:5672/'#g" /home/antoinet/src/tucat/config/settings/common.py 
+#sed -i "s#BROKER_URL.*#BROKER_URL = 'amqp://tucat:${RBPASS}@localhost:5672/'#g" /home/antoinet/src/tucat/config/settings/common.py 
+sed -i "s#BROKER_URL.*#BROKER_URL = 'amqp://${APPUSER}:${RBPASS}@localhost:5672/'#g" ${APPHOME}/tucat/config/settings/common.py 
 
 echo "=>"
 echo "=> Done!"
 echo "=> ROOT Password: ${ROOTPASS} "  
-echo "=> Creating POSTGRESQL admin user with ${PASS} password"
+echo "=> Creating POSTGRESQL ${APPUSER} user with ${PASS} password"
 
